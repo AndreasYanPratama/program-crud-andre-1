39019132400fa1f809d3e57d62d75cd429e16292
Andreas Yan Pratama <andreasyanpratama@gmail.com>
2022-02-25 12:45:35 +0700

    first

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,10 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <!-- My CSS -->
-    <!-- <link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.min.css"> -->
-    <!-- <script src="https://cdnjs.com/libraries/pdf.js"></script> -->
-    <!-- <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css"> -->
+
     <title><?php echo $title; ?></title>
 </head>
 
@@ -25,10 +22,6 @@
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a class="nav-link" href="<?= base_url(); ?>">Home <span class="sr-only">(current)</span></a>
-                    <!-- <a class="nav-link" href="<?= base_url(); ?>students">Mahasiswa</a> -->
-                    <!-- <a class="nav-link" href="<?= base_url(); ?>peoples">Peoples</a> -->
-                    <!-- <a class="nav-link" href="#">About</a> -->
-
                 </div>
             </div>
         </div>
